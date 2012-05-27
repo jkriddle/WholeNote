e4c405c76ff11c34280c0b7a5d657484cdb6f4be
Joshua Riddle <josh@theriddlebrothers.com>
2012-05-26 22:05:19 -0400

    Initial style layout.

---
+++ Src/WholeNote.App/ViewModel/MainViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System.Windows;
+using GalaSoft.MvvmLight;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+
+namespace WholeNote.App.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm/getstarted
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+
+        #region Fields
+
+        private ICommand _exitCommand;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            if (IsInDesignMode)
+            {
+                // Code runs in Blend --> create design time data.
+            }
+            else
+            {
+                // Code runs "for real"
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Saves current data and exits application
+        /// </summary>
+        public ICommand ExitCommand
+        {
+            get
+            {
+                if (_exitCommand == null)
+                {
+                    _exitCommand = new RelayCommand(() => Application.Current.Shutdown());
+                }
+                return _exitCommand;
+            }
+        }
+
+        #endregion
+
+        ////public override void Cleanup()
+        ////{
+        ////    // Clean up if needed
+
+        ////    base.Cleanup();
+        ////}
+    }
+}
